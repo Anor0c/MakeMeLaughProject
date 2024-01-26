bb815375685da35a969f396b8d2795f9a9da0473
Anor0c <94901070+Anor0c@users.noreply.github.com>
2024-01-26 20:20:41 +0100

    UIBarMarche

--- GGJam_2024/Assets/Scripts/UIBar.cs
+++ GGJam_2024/Assets/Scripts/UIBar.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; 
 
 public class UIBar : MonoBehaviour
 {
+    [SerializeField]private float maxValue = 100f;
+    [SerializeField]private float currentValue; 
+
+    [SerializeField] private Image innerBar; 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentValue = maxValue; 
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnUpdateValue(float delta)
     {
-        
+        currentValue -= delta; 
+        innerBar.fillAmount = currentValue / maxValue; 
     }
 }
