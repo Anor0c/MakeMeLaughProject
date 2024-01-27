244bbeaedbcc86049493f2dd12f318982b037831
Anor0c <94901070+Anor0c@users.noreply.github.com>
2024-01-27 16:11:47 +0100

    ShameFixed

--- GGJam_2024/Assets/Scripts/LookingCrowd.cs
+++ GGJam_2024/Assets/Scripts/LookingCrowd.cs
@@ -4,33 +4,33 @@ using UnityEngine;
 
 public class LookingCrowd : MonoBehaviour
 {
-    [SerializeField] private float damage = 1f; 
+    [SerializeField] private float damage = 1f;
     [SerializeField] private float minCalmTimer = 0.5f, maxCalmTimer = 1.5f;
     [SerializeField] private float minLookingTimer = 0.5f, maxLookingTimer = 1.5f;
-    [SerializeField] private float activeTimer, silenceWarning; 
+    [SerializeField] private float activeTimer, silenceWarning;
     [SerializeField] private bool isLooking = false;
     private const float damageMultiplier = 0.01f;
 
     private SpriteRenderer crowdSprite;
     private JokeBehaviour player;
-    private AudioSource crowdAudio; 
+    private AudioSource crowdAudio;
     [SerializeField] private UIBar shameBar;
 
     void Start()
     {
         crowdSprite = GetComponent<SpriteRenderer>();
-        crowdAudio = GetComponent<AudioSource>(); 
+        crowdAudio = GetComponent<AudioSource>();
         player = FindObjectOfType<JokeBehaviour>();
-        damage *= damageMultiplier; 
+        damage *= damageMultiplier;
         ChooseNextRoutine();
     }
 
     private void ChooseNextRoutine()
     {
-        StopAllCoroutines(); 
+        StopAllCoroutines();
         if (!isLooking)
         {
-            StartCoroutine("NoLookRoutine"); 
+            StartCoroutine("NoLookRoutine");
         }
         if (isLooking)
         {
@@ -39,32 +39,32 @@ public class LookingCrowd : MonoBehaviour
     }
     private void Update()
     {
-        if (player.State == PlayerStates.Joking && isLooking)
+
+        if (isLooking && player.IsJoking)
         {
-            Debug.Log("SHAME!!!!"); 
             shameBar.UpdateValue(damage);
         }
     }
     private IEnumerator NoLookRoutine()
     {
         crowdSprite.color = Color.gray;
-        crowdAudio.Play(); 
+        crowdAudio.Play();
         activeTimer = Random.Range(minCalmTimer, maxCalmTimer);
         yield return new WaitForSeconds(activeTimer);
         crowdAudio.Pause();
-        yield return new WaitForSeconds(silenceWarning); 
+        yield return new WaitForSeconds(silenceWarning);
         isLooking = true;
-        ChooseNextRoutine(); 
+        ChooseNextRoutine();
         yield return null;
-    } 
+    }
     private IEnumerator LookingRoutine()
     {
         crowdSprite.color = Color.red;
-        crowdAudio.Pause(); 
+        crowdAudio.Pause();
         activeTimer = Random.Range(minLookingTimer, maxLookingTimer);
         yield return new WaitForSeconds(activeTimer);
         isLooking = false;
-        ChooseNextRoutine(); 
+        ChooseNextRoutine();
         yield return null;
     }
 }
