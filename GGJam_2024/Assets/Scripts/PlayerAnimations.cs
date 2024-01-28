a8d9eac2720fe43a5da4fb40d19c4dceae3785e8
Laurie Liégois <laurie.liegois@gmail.com>
2024-01-28 17:45:33 +0100

    Psuh

--- GGJam_2024/Assets/Scripts/PlayerAnimations.cs
+++ GGJam_2024/Assets/Scripts/PlayerAnimations.cs
@@ -4,18 +4,22 @@ using UnityEngine;
 
 public class PlayerAnimations : MonoBehaviour
 {
-    private Animator anim;
+   /* private Animator anim;
 
 
     void Start()
     {
         anim = GetComponent<Animator>();
     }
-    private readonly int idle = Animator.StringToHash("Idle"); 
-    private readonly int hit = Animator.StringToHash("Hit"); 
+    private readonly int idle = Animator.StringToHash("Kid-Idle"); 
+    private readonly int hit = Animator.StringToHash("LVL1-Kid-Hit"); 
+    private readonly int hit = Animator.StringToHash("LVL1-Kid-Imitation"); 
+    private readonly int hit = Animator.StringToHash("LVL1-Kid-Prout"); 
+    private readonly int hit = Animator.StringToHash("LVL1-Kid-FunnyFace"); 
     public void OnPlayerHit()
     {
         anim.CrossFade(hit, 0, 0); 
 
     }
+   */
 }
