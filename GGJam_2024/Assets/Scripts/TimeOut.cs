ee1d1edd0cb32ae1fcc4d116a338c1d143be837c
Andre Yu <yua.animation@gmail.com>
2024-01-28 01:01:31 +0100

    countdown

---
+++ GGJam_2024/Assets/Scripts/TimeOut.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class TimeOut : MonoBehaviour
+{
+    public float timeRemaining = 0;
+    public bool timeISRunning = true;
+    public TMP_Text timeText;
+    private MainMenu menu;
+    [SerializeField] private Image uifill;
+    public float timestart = 0;
+
+    void Start()
+    {
+        timeISRunning = true;
+        menu = FindAnyObjectByType<MainMenu>();
+    }
+    void Update()
+    {
+        if (timeISRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                DisplayTime(timeRemaining);
+            }
+            else if (timeRemaining <= 2)
+            { 
+                DisplayTime(timeRemaining);
+            }
+        }
+
+    }
+    void DisplayTime(float timeToDisplay)
+    {
+        timeToDisplay -= 1;
+        if (timeRemaining <= 2)
+        {
+            menu.Startgame();
+        }
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+        timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        uifill.fillAmount = Mathf.InverseLerp(-2, timestart,timeRemaining);
+    }
+}
